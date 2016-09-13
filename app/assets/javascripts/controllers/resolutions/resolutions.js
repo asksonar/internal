b3b9c17332b494cc4c050bdf309ef113960451c8
Stacy Huang <stacy.y.huang@gmail.com>
2016-09-13 14:06:58 -0700

    Apply resolution using HTTParty

--- app/assets/javascripts/controllers/resolutions/resolutions.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-  if (!$('#resolutions-show').length) {
-    return;
-  }
-
-  var resolutionsController = new ResolutionsController({
-    btnSaveResolution: $('#btn-save-resolution')
-  });
-});
--- app/controllers/resolutions_controller.rb
+++ app/controllers/resolutions_controller.rb
@@ -14,12 +14,74 @@ class ResolutionsController < ApplicationController
 
     if !updates['aircraft_history'].nil?
       @update_type = 'aircraft_history'
-      @existing_data = merge_data['aircraft_history']
+      @merge_data = merge_data['aircraft_history']
       @new_data = updates['aircraft_history']
     else
       @update_type = 'aircraft'
-      @existing_data = merge_data['aircraft']
+      @merge_data = merge_data['aircraft']
       @new_data = updates['aircraft']
     end
+  end
+
+  def update
+    airsonar = Airsonar.new()
+    approved_updates = updates.select { |key, value| value == 'approve' }.keys
+    rejected_updates = updates.select { |key, value| value == 'reject' }.keys
+
+    if approved_updates.empty?
+      response = airsonar.rejectResolution(params[:id])
+    else
+      options = Hash.new { |h,k| h[k] = Hash.new(&h.default_proc) }
+
+      if approved_updates.present? && rejected_updates.present?
+        merge_data.each do |key, value|
+          options[:merge_data][update_type][key] = value
+        end
+
+        approved_updates.each do |key, value|
+          options[:updates][update_type][key] = new_data[key]
+        end
+      end
+
+      response = airsonar.applyResolution(params[:id], options)
+    end
+
+    redirect_to resolutions_path
+  end
+
+  private
+
+  def merge_data
+    update_params[:merge_data]
+  end
+
+  def new_data
+    update_params[:new_data]
+  end
+
+  def updates
+    update_params[:updates]
+  end
+
+  def update_type
+    update_params[:update_type]
+  end
+
+  def update_params
+    params.require(:resolutions).permit(
+      :update_type,
+      :merge_data => [
+        :id, :model, :msn, :delivery_date, :aircraft_status, :aircraft_type, :registration,
+        :engine_model, :engine_variant,:operator, :build_year, :aircraft_age, :line_number
+      ],
+      :new_data => [
+        :aircraft_status, :aircraft_type, :registration, :engine_model, :engine_variant,
+        :operator, :build_year, :aircraft_age, :line_number
+      ],
+      :updates => [
+        :aircraft_status, :aircraft_type, :registration, :engine_model, :engine_variant,
+        :operator, :build_year, :aircraft_age, :line_number
+      ]
+    )
   end
 end
